82b7fa059352622a1f1f22f1de7a0d3ebe1bc1b0
Johan Brook <johan.jvb@gmail.com>
2014-02-13 22:17:07 +0100

    Styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,23 @@ module ApplicationHelper
 	  end
 	end
 
+	def body_classes(classes=nil)
+	    ary = []
+	    ary << controller.controller_name
+	    ary << controller.action_name
+	    ary << 'mobile' if mobile_agent?
+	    
+	    unless classes.nil?
+	      method = classes.is_a?(Array) ? :concat : :<<
+	      ary.send method, classes
+	    end
+	 
+	    ary.join(' ')
+	  end
+	  
+	  def mobile_agent?
+	    return true if params[:mobile] == "1"
+	    request.user_agent =~ /Mobile|webOS/
+	  end 
+
 end
