fa517e227ec1cf229911dcaa850761d6bec17afb
Johan Brook <johan.jvb@gmail.com>
2014-02-13 22:41:46 +0100

    Add user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@users = User.all
+	end
+
+	def show
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+	  params.require(:user).permit(:username, :email, :level, :password, :password_confirmation)
 	end
 end
