84a4ca027f3571bdf884cf5b3aa29399dd04dc28
Johan Brook <johan.jvb@gmail.com>
2014-03-17 12:41:15 +0100

    Use URL scheme to link to a song

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :authorize
 
   def home
-
+    @song = Song.first
   end
 
   def download
--- app/models/song.rb
+++ app/models/song.rb
@@ -5,4 +5,9 @@ class Song < ActiveRecord::Base
 	def cover_url
 		cover || "http://placehold.it/300x300"
 	end
+
+	def song_uri
+			# Format: "ic:song=Amazing%20grace.mid"
+			"ic:song=#{Rack::Utils.escape(title)}.mid"
+	end
 end
