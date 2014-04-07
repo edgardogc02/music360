a7abb2359efef27fcf388de4350893cd1c58dfdb
edgardo <edgardo@loventine.com>
2014-04-07 11:27:51 +0200

    * Modified left menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	def nav_link(link_text, link_path, options)
-	  class_name = current_page?(link_path) ? 'current' : ''
+	  class_name = current_page?(link_path) ? 'current' : 'inactive'
 
 	  content_tag(:li, class: class_name) do
 	    link_to link_path do
@@ -11,32 +11,36 @@ module ApplicationHelper
 	end
 
 	def body_classes(classes=nil)
-	    ary = []
-	    ary << controller.controller_name
-	    ary << controller.action_name
-	    ary << 'mobile' if mobile_agent?
+    ary = []
+    ary << controller.controller_name
+    ary << controller.action_name
+    ary << 'mobile' if mobile_agent?
 
-	    unless classes.nil?
-	      method = classes.is_a?(Array) ? :concat : :<<
-	      ary.send method, classes
-	    end
-
-	    ary.join(' ')
-	  end
-
-	  def mobile_agent?
-	    return true if params[:mobile] == "1"
-	    request.user_agent =~ /Mobile|webOS/
-	  end
-
-		def is_current_user?(user)
-			current_user and current_user.id == user.id
-		end
-
-    def js_scripts
-      if content_for?(:js_scripts)
-        content_for(:js_scripts)
-      end
+    unless classes.nil?
+      method = classes.is_a?(Array) ? :concat : :<<
+      ary.send method, classes
     end
+
+    ary.join(' ')
+  end
+
+  def mobile_agent?
+    return true if params[:mobile] == "1"
+    request.user_agent =~ /Mobile|webOS/
+  end
+
+	def is_current_user?(user)
+		current_user and current_user.id == user.id
+	end
+
+  def js_scripts
+    if content_for?(:js_scripts)
+      content_for(:js_scripts)
+    end
+  end
+
+  def display_left_menu_second_level?(allowed_paths)
+    allowed_paths.any? { |allowed_path| current_page?(allowed_path) }
+  end
 
 end
