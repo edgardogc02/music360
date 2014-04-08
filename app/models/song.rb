2535eba41bf2070d71f1d75f3352a625152dc5e5
edgardo <edgardo@loventine.com>
2014-04-08 10:04:04 +0200

    * comment should not be mandatory in song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,6 @@ class Song < ActiveRecord::Base
   validates :length, presence: true
   validates :difficulty, presence: true
   validates :arranger_userid, presence: true
-  validates :comment, presence: true
   validates :status, presence: true
   validates :onclient, presence: true
   validates :published_at, presence: true
