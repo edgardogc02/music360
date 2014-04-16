700d97f765212666d8c08238f23e6e5e7af87c2d
edgardo <edgardo@loventine.com>
2014-04-16 16:26:15 +0200

    * Changed storage from carrierwave to ftp

--- app/uploaders/user_imagename_uploader.rb
+++ app/uploaders/user_imagename_uploader.rb
@@ -7,14 +7,14 @@ class UserImagenameUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :ftp
+#  storage :file
+  storage :ftp
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/images_users/#{model.id}"
+    "images_users/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
