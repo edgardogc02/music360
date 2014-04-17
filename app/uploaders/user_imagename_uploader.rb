3c9a7ae425000ebb52d0cc4e9f3e0e63e58cad31
edgardo <edgardo@loventine.com>
2014-04-17 10:21:39 +0200

    * Changed storage to file in carrierwave and development

--- app/uploaders/user_imagename_uploader.rb
+++ app/uploaders/user_imagename_uploader.rb
@@ -7,8 +7,7 @@ class UserImagenameUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-#  storage :file
-  storage :ftp
+  # storage :ftp # this is setup in the carrierwave.rb initializer file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,4 +15,12 @@ if Rails.env.test? or Rails.env.cucumber?
     config.storage = :file
     config.enable_processing = false
   end
+elsif Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :ftp
+  end
 end
