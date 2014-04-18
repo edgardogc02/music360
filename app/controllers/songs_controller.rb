b6693a021afe23ca9203e38dfbc243e00191f359
edgardo <edgardo@loventine.com>
2014-04-18 11:32:14 +0200

    * Added fields to edit song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,7 +32,7 @@ class SongsController < ApplicationController
 	private
 
   def song_params
-    params.require(:song).permit(:cover)
+    params.require(:song).permit(:title, :writer, :length, :difficulty, :arranger_userid, :comment, :status, :onclient, :published_at, :cover)
   end
 
 	def set_song
