bb65840f2a770be4e1a32a05f4a9bcb778480eae
edgardo <edgardo@loventine.com>
2014-04-18 17:08:28 +0200

    * Changed song cover image path

--- app/uploaders/song_cover_uploader.rb
+++ app/uploaders/song_cover_uploader.rb
@@ -13,7 +13,7 @@ class SongCoverUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "songart/#{model.id}"
+    "songart"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -42,7 +42,7 @@ class SongCoverUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "image.jpg" if original_filename
+    "#{model.id}.jpg" if original_filename
   end
 
 end
