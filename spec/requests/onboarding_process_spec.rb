3d4f7cc2185e14133e1c4ad7cb35cffd738faf50
edgardo <edgardo@loventine.com>
2014-04-24 15:34:21 +0200

    * Onboarding process

---
+++ spec/requests/onboarding_process_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "OnboardingProcess" do
+
+  before(:each) do
+    @song = create(:song)
+  end
+
+  context "sign in user" do
+    before(:each) do
+      @user = login
+    end
+
+    it "should display header with links in the welcome page" do
+      visit welcome_path
+      has_header_with_links
+    end
+
+    it "should display header with links in the user instruments page" do
+      visit edit_user_instrument_path(@user.id, next: "user_groupies")
+      has_header_with_links
+    end
+
+    it "should display header with links in the user groupies page" do
+      visit user_groupies_path
+      has_header_with_links
+    end
+
+    def has_header_with_links
+      page.should have_link "Welcome", href: welcome_path
+      page.should have_link "Choose Instrument", href: edit_user_instrument_path(@user.id, next: "user_groupies")
+      page.should have_link "Connect with Groupies", href: user_groupies_path
+    end
+  end
+
+  context "not sign in user" do
+  end
+
+end
