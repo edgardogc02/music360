9ddec13066671bb91d7f5df042de524b09dbfaae
leo <leo@loventine.com>
2014-04-27 11:21:51 -0300

    * Welcome message FB login fixing

--- app/controllers/user_omniauth_credentials_controller.rb
+++ app/controllers/user_omniauth_credentials_controller.rb
@@ -1,9 +1,11 @@
+#encoding: utf-8
 class UserOmniauthCredentialsController < ApplicationController
 
   def create
     user = User.from_omniauth(request.env["omniauth.auth"], request.remote_ip)
     if user and !user.deleted?
       signin_user(user)
+      flash[:welcome] = "Hi #{user.username}!"
       redirect_to root_path
     else
       redirect_to login_path
