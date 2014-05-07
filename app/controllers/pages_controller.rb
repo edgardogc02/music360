a145573384798b4b5c26c8e123aa3be67c6057a0
leo <leo@loventine.com>
2014-05-07 07:34:07 -0300

    * Modified homepage. Added 4 songs and users.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @song = Song.first.decorate
+    @regular_users = User.not_deleted.exclude(current_user.id).limit(4)
+    @songs = SongDecorator.decorate_collection(Song.free.by_popularity.limit(4))
   end
 
   def download
