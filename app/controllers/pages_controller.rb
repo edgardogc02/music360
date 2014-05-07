a10480fab2f1f0a83043e676bb34e92ffdc58d3a
leo <leo@loventine.com>
2014-05-07 15:17:17 -0300

    * Modified challenges list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   before_action :authorize
 
   def home
-    @song = Song.first.decorate
     @regular_users = User.not_deleted.exclude(current_user.id).limit(4)
     @songs = SongDecorator.decorate_collection(Song.free.by_popularity.limit(4))
   end
