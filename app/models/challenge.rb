2f9cb926138b80405a02574838771fd3acec154c
edgardo <edgardo@loventine.com>
2014-05-14 13:06:39 +0100

    * challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,20 +5,17 @@ class Challenge < ActiveRecord::Base
   validates :song_id, presence: true
   validates :instrument, presence: true
   validates :public, inclusion: {in: [true, false]}
-  validates :finished, inclusion: {in: [true, false]}
 
   validate :challenged_and_finished, :on => :create
   validate :no_own_challenge, :on => :create
 
 	belongs_to :challenger, class_name: "User", foreign_key: "challenger_id"
 	belongs_to :challenged, class_name: "User", foreign_key: "challenged_id"
-  belongs_to :winner, class_name: "User", foreign_key: "winner"
 	belongs_to :song
 
   before_create :fill_in_extra_fields
 
   scope :public, -> { where(public: true) }
-	scope :open, -> { where(finished: false) }
   scope :pending_by_challenger, -> { where('score_u1 = 0') }
   scope :pending_by_challenged, -> { where('score_u2 = 0') }
 	scope :pending_only_by_challenged, -> { where('score_u1 > 0 and score_u2 = 0') }
@@ -55,6 +52,7 @@ class Challenge < ActiveRecord::Base
     user == self.challenged
   end
 
+  # TODO: refactor the following 3 methods
   def self.pending_for_user(user, opts={})
     sql = user.challenges.pending_only_by_challenged.to_sql + " UNION " + user.proposed_challenges.pending_only_by_challenger.to_sql
     sql = "SELECT * FROM (#{sql}) a "
@@ -64,7 +62,7 @@ class Challenge < ActiveRecord::Base
     Challenge.find_by_sql(sql)
   end
 
-  def self.not_played_for_user(user, opts={})
+  def self.not_played_by_user(user, opts={})
     sql = user.challenges.pending_by_challenger.to_sql + " UNION " + user.proposed_challenges.pending_by_challenged.to_sql
 
     sql = "SELECT * FROM (#{sql}) a "
@@ -95,16 +93,16 @@ class Challenge < ActiveRecord::Base
 	private
 
 	def challenged_and_finished
-    errors.add(:finished, "You already have an open challenge for that song with that user") if Challenge.where(challenger_id: self.challenger_id, challenged_id: self.challenged_id, song_id: self.song_id, finished: false).count > 0
+    errors.add(:challenged_id, "You already have an open challenge for that song with that user") if Challenge.where(challenger_id: self.challenger_id, challenged_id: self.challenged_id, song_id: self.song_id).where("score_u1 = 0 OR score_u2 = 0").count > 0
 	end
 
   def no_own_challenge
-    errors.add(:finished, "You can't challenge your self") if self.challenger and self.challenged and self.challenger == self.challenged
+    errors.add(:challenger_id, "You can't challenge your self") if self.challenger and self.challenged and self.challenger == self.challenged
   end
 
   def fill_in_extra_fields
-    self.score_u1 = 0
-    self.score_u2 = 0
+    self.score_u1 = 0 if self.score_u1.nil?
+    self.score_u2 = 0 if self.score_u2.nil?
   end
 
 end
