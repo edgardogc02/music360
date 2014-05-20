d3027cf5caa7a5e76812f3dd3809cbe338e0a144
edgardo <edgardo@loventine.com>
2014-05-20 16:06:00 +0100

    * Added created_by column to users * Added created_by column to songs * Added created_by column to instruments * Fixed location of images for users, songs and instruments in test-instrumentchamp

--- spec/support/user_macros.rb
+++ spec/support/user_macros.rb
@@ -9,6 +9,7 @@ module UserMacros
     user.premium.should be_true
     user.premium_until.to_date.should eq(3.months.from_now.to_date) # remove seconds
     user.countrycode.should_not be_blank
+    user.created_by.should_not be_blank
     # user.city.should_not be_blank # TODO: Check how to test this in test mode is always blank
 
     if check_oauth_uid
