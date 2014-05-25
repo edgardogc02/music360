2c89abbfacfb099d76d50625eea20c74e00cc770
leo <leo@loventine.com>
2014-05-25 18:12:19 -0300

    * Home: updated quick start challenge users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,8 @@ class PagesController < ApplicationController
   before_action :authorize, except: [:help]
 
   def home
-    users = User.not_deleted.exclude(current_user).search_user_relationships(current_user)
-    if users.count < 4
-      users = User.not_deleted.exclude(current_user.id)
-    end
-    @users = UserChallengeDecorator.decorate_collection(users.limit(4))
+    users = User.not_deleted.exclude(current_user).search_user_relationships(current_user).limit(4) | User.not_deleted.exclude(current_user).limit(4)
+    @users = UserChallengeDecorator.decorate_collection(users.take(4))
     @songs = SongQuickStartDecorator.decorate_collection(Song.free.by_popularity.limit(4))
     
     @instruments = Instrument.visible
