200201e2266669d388ed7bb75a56b76784edd048
leo <leo@loventine.com>
2014-05-25 14:53:52 -0300

    * Home page: show people that I have an existing relationship with

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   before_action :authorize, except: [:help]
 
   def home
-    @users = UserChallengeDecorator.decorate_collection(User.not_deleted.exclude(current_user.id).limit(4))
+    users = User.not_deleted.exclude(current_user).search_user_relationships(current_user)
+    if users.count < 4
+      users = User.not_deleted.exclude(current_user.id)
+    end
+    @users = UserChallengeDecorator.decorate_collection(users.limit(4))
     @songs = SongQuickStartDecorator.decorate_collection(Song.free.by_popularity.limit(4))
     
     @instruments = Instrument.visible
