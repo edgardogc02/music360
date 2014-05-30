762bbb4d62bb0aa7fcd685ba99cfc17557c8b77a
edgardo <edgardo@loventine.com>
2014-05-30 14:47:32 +0100

    * Song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,7 +37,7 @@ class SongsController < ApplicationController
 	private
 
   def song_params
-    params.require(:song).permit(:title, :writer, :length, :difficulty, :arranger_userid, :comment, :status, :onclient, :published_at, :cover)
+    params.require(:song).permit(:title, :writer, :length, :difficulty, :arranger_userid, :comment, :status, :onclient, :published_at, :cover, :visible)
   end
 
 	def set_song
