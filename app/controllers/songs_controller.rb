c78613e9212802efd988ec5c6cf108a47ab86de9
leo <leo@loventine.com>
2014-06-03 18:44:00 -0300

    * UI updated (in progress)

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,9 @@ class SongsController < ApplicationController
 
 	def show
 	  @song = SongDecorator.decorate(@song)
+	  @more_songs = SongChallengeDecorator.decorate_collection(Song.free.visible.by_popularity.limit(4))
+	  users = User.not_deleted.exclude(current_user).search_user_relationships(current_user).limit(4) | User.not_deleted.exclude(current_user).limit(4)
+    @users = UserChallengeDecorator.decorate_collection(users.take(4))
 	end
 
   def edit
