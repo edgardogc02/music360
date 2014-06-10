f828b2dc22f75ec68301f3dd237620a011250ebe
leo <leo@loventine.com>
2014-06-10 11:12:13 -0300

    * Updated artist challenge tabs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,9 +9,9 @@ class ArtistsController < ApplicationController
 	def show
 	  @songs = SongDecorator.decorate_collection(@artist.songs.limit(4))    
     users = User.not_deleted.exclude(current_user).search_user_relationships(current_user).limit(4) | User.not_deleted.exclude(current_user).limit(4)
-    @users = UserChallengeDecorator.decorate_collection(users.take(4))
-    @challenges = ChallengeDecorator.decorate_collection(Challenge.finished.limit(4))
-    
+    @users = UserChallengeDecorator.decorate_collection(users.take(4))    
+    @challenges = ChallengeDecorator.decorate_collection(Challenge.where("song_id IN (?)", @artist.songs.pluck(:id)).finished.limit(4))
+   
     @more_songs = SongChallengeDecorator.decorate_collection(Song.free.not_user_created.by_popularity.limit(4))
 	end
 
