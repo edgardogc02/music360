7c8e50463b9ca2a326a53ba5f697d20d937a20f7
edgardo <edgardo@loventine.com>
2014-06-19 14:30:09 +0100

    * removed debug from artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -10,11 +10,11 @@ class Artist < ActiveRecord::Base
 	has_many :songs
 
 	def bio
-#	  Echowrap.artist_biographies(name: self.title, results: 2, start: 1, license: "cc-by-sa").first if Echowrap.artist_biographies(name: self.title)
+	  Echowrap.artist_biographies(name: self.title, results: 2, start: 1, license: "cc-by-sa").first if Echowrap.artist_biographies(name: self.title)
 	end
 
 	def public_image_url
-#	  Echowrap.artist_images(name: self.title).first if Echowrap.artist_images(name: self.title)
+	  Echowrap.artist_images(name: self.title).first if Echowrap.artist_images(name: self.title)
   end
 
 end
