12fa87bb951952ae91c9d9ed4af9e02cd27f4dd5
edgardo <edgardo@loventine.com>
2014-07-07 15:45:15 +0100

    * mandrill template

--- app/mailers/mandrill_template_email_notifier.rb
+++ app/mailers/mandrill_template_email_notifier.rb
@@ -5,7 +5,7 @@ class MandrillTemplateEmailNotifier < MandrillMailer::TemplateMailer
   def test_mandrill_template(user)
     mandrill_mail template: 'Test',
                   subject: 'Mandrill template email test!',
-                  to: {email: user.email, name: user.name}
+                  to: {email: user.email, name: user.username}
   end
 
 end
