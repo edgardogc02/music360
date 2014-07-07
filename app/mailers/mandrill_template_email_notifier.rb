593cb70cf7eb44d6f945cd2e67e75a4c2b5b8dd9
edgardo <edgardo@loventine.com>
2014-07-07 15:24:45 +0100

    * mandrill template

--- app/mailers/mandrill_template_email_notifier.rb
+++ app/mailers/mandrill_template_email_notifier.rb
@@ -1,11 +1,11 @@
 class MandrillTemplateEmailNotifier < MandrillMailer::TemplateMailer
 
-  default from: "InstrumentChamp <no-reply@instrumentchamp.com>"
+#  default from: "InstrumentChamp <no-reply@instrumentchamp.com>"
 
   def test_mandrill_template(user)
     mandrill_mail template: 'Test',
                   subject: 'Mandrill template email test!',
-                  to: "edgardo.cabanillas@instrumentchamp.com"
+                  to: user.email
   end
 
 end
