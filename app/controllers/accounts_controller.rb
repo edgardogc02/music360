ff363f98c11311daa8a94d6249602479c0a19834
leo <leo@loventine.com>
2014-08-05 16:22:10 -0300

    * Updated cancel subscription

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,13 +3,13 @@ class AccountsController < ApplicationController
 	before_action :set_user
 
   def overview
-    @user = User.find(current_user.id)
   end
 
   def profile
   end
   
   def subscription
+    @subscription = UserPremiumSubscription.find_by user_id: @user.id
   end
   
   def set_user
