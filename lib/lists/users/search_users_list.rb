03ff8f3b2e9988fb32459c3385aabf4ece80f138
edgardo <edgardo@loventine.com>
2014-08-21 10:26:48 +0100

    * Fixed user search

--- lib/lists/users/search_users_list.rb
+++ lib/lists/users/search_users_list.rb
@@ -13,7 +13,7 @@ class SearchUsersList < PaginatedUsersList
   end
 
   def users
-    @users ||= UserDecorator.decorate_collection(User.by_username_or_email(username_or_email).search_user_relationships(current_user).exclude(current_user).page page)
+    @users ||= UserDecorator.decorate_collection(User.by_username_or_email(username_or_email).exclude(current_user).page page)
   end
 
   def username_or_email
