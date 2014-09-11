5bc85dd31323c014c59a23e31a51b75ac85bbc8e
edgardo <edgardo.cabanillas@instrumentchamp.com>
2014-09-11 10:10:59 +0200

    * Removed user created songs from free songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -26,7 +26,7 @@ class Song < ActiveRecord::Base
   has_many :song_scores, dependent: :destroy
 
   scope :by_title, ->(title) { where('title LIKE ?', '%'+title+'%') }
-  scope :free, -> { where('cost IS NULL OR cost = 0') }
+  scope :free, -> { where('(cost IS NULL OR cost = 0) AND user_created != 1 ') }
   scope :paid, -> { where('cost > 0') }
   scope :by_popularity, -> { joins('LEFT JOIN songratings ON songratings.song_id = songs.id').group('case when songratings.song_id is null then songs.id else songratings.song_id end').order('AVG(songratings.rating) DESC, songs.id') }
   scope :by_published_at, -> { order('published_at DESC') }
