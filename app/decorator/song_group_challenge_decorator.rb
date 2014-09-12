04cca070a88f9d182034d699e48d3dad878e31d3
edgardo <edgardo.cabanillas@instrumentchamp.com>
2014-09-12 11:20:33 +0200

    * Fixed bug with group create challenge

--- app/decorator/song_group_challenge_decorator.rb
+++ app/decorator/song_group_challenge_decorator.rb
@@ -1,6 +1,6 @@
 class SongGroupChallengeDecorator < SongChallengeDecorator
 
-  def challenge_button(params)
+  def challenge_button(params, size="")
     if display_challenge_button?
       h.action_button(h.new_group_challenge_path(Group.find(params[:group_id]), song_id: model.id), 'Challenge', {class: challenge_class_attr, id: "challenge_#{model.id}", data: {song_id: model.id, song_name: model.title}})
     end
