4c62b5c5bed1f4a2ad1b318001c9ac7dc2ec0249
magnuswillner <magnus.willner@instrumentchamp.com>
2014-10-16 12:04:32 +0200

    Another try. With gsub instead of sub.

--- app/uploaders/midi_uploader.rb
+++ app/uploaders/midi_uploader.rb
@@ -15,7 +15,7 @@ class MidiUploader < Uploader
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    original_filename.sub("_", " ") if original_filename
+    original_filename.gsub("_", " ") if original_filename
   end
 
 end
