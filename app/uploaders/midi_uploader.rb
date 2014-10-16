44408fddd15f2097d88625202e26c7cb074b40d3
magnuswillner <magnus.willner@instrumentchamp.com>
2014-10-16 11:52:15 +0200

    Another try

--- app/uploaders/midi_uploader.rb
+++ app/uploaders/midi_uploader.rb
@@ -15,9 +15,7 @@ class MidiUploader < Uploader
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    #original_filename.sub(" ", "_") if original_filename
     original_filename.sub("_", " ") if original_filename
-    original_filename.sub("-", " ") if original_filename
   end
 
 end
