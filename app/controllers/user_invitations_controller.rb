d1a4228402d252a56d3a6b0064753af83f1e1726
edgardo <edgardo@loventine.com>
2014-11-27 17:01:36 +0100

    * Added pid to user twitt message

--- app/controllers/user_invitations_controller.rb
+++ app/controllers/user_invitations_controller.rb
@@ -6,7 +6,7 @@ class UserInvitationsController < ApplicationController
 
     if params[:tweet] and params[:tweet_text]
       @user_twitter_account = UserTwitterAccount.new(current_user)
-      @tweet = @user_twitter_account.tweet("#{params[:tweet_text][0..104]} #{root_url}")
+      @tweet = @user_twitter_account.tweet("#{params[:tweet_text][0..104]} #{root_url}?pid=#{current_user.id}")
     end
 
     if params[:follow_artist]
